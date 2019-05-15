50776dd21eb3e5c1d8e5f6c91d495e98f00e4df6
Enrique <enrique.gomez@telecom-paristech.fr>
2019-05-15 15:59:29 +0200

    WIP

--- TD4_EventProc/pixel.cpp
+++ TD4_EventProc/pixel.cpp
@@ -63,3 +63,8 @@ void sc_trace( sc_trace_file* _f, const Pixel_t& _Pixel, const std::string& _s )
    sc_trace( _f, _Pixel.G, _s + "G" );
    sc_trace( _f, _Pixel.B, _s + "B" );
 }
+
+int sc_main(int argc, char * argv[])
+{
+   return 0;
+}
