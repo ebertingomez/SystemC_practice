96ea2cc93be06c41918fbc6e3133d881874f80af
Enrique <enrique.gomez@telecom-paristech.fr>
2019-06-04 16:26:10 +0200

    Completion Temp Model

--- TD5_PGCD/mod_pgcd.cpp
+++ TD5_PGCD/mod_pgcd.cpp
@@ -1,5 +1,5 @@
 #include <systemc.h>
-#include <stdint.h>
+#include <stdlib.h> 
 
 SC_MODULE(mod_pgcd) {
     // Inputs
@@ -12,6 +12,11 @@ SC_MODULE(mod_pgcd) {
     sc_out< bool > ready;
     sc_out< sc_uint<8> > pgcd;
 
+   SC_CTOR(mod_pgcd):valid("valid"),A("A"),B("B"),ready("ready"),pgcd("pgcd")
+   {
+       SC_CTHREAD(mthread,clk.pos());
+   }
+
     void mthread(){
         unsigned int current_pgcd;
         for(;;){
@@ -19,42 +24,42 @@ SC_MODULE(mod_pgcd) {
             if (valid.read()){
                 current_pgcd = compute_pgcd(A.read().to_int(),B.read().to_int());
                 pgcd.write(sc_uint<8>(current_pgcd));
-                ready = 1;
+                ready.write(1);
                 wait(1);
-                ready = 0;
+                ready.write(0);
             }
         }
     }
 
     int compute_pgcd(unsigned int a,unsigned int b){
         int bigger = (a>b) ? a : b;
+        wait(1);
         int smaller = (a<b) ? a : b;
+        wait(1);
         int diff;
         do {
+            wait(1);
             diff = bigger - smaller;
+            wait(1);
             bigger = (diff>smaller) ? diff : smaller;
+            wait(1);
             smaller = (diff<smaller) ? diff : smaller;
+            wait(1);
         }
         while (diff >0);
         return bigger;
     }
-
-   SC_CTOR(mod_pgcd):valid("valid"),A("A"),B("B"),ready("ready"),pgcd("pgcd")
-   {
-       SC_CTHREAD(mthread,clk.pos());
-   }
 };
 
 int sc_main(int argc, char * argv[])
 {
-    sc_clock clk("clk",10,SC_NS);
+    sc_clock clk("clk",40,SC_NS);
     sc_signal< bool> valid;
     sc_signal< sc_uint<8> > A;
     sc_signal< sc_uint<8> > B;
     
     sc_signal< bool > ready;
     sc_signal< sc_uint<8> > pgcd;
-
 
     mod_pgcd module_pgcd("module_pgcd");
     module_pgcd.clk(clk);
@@ -65,28 +70,38 @@ int sc_main(int argc, char * argv[])
     module_pgcd.pgcd(pgcd);
 
     sc_trace_file *trace_f;
-    trace_f = sc_create_vcd_trace_file ("pgcd_1");
-    trace_f->set_time_unit(5,SC_NS);
-    sc_trace(trace_f, module_pgcd.pgcd, "pgcd");
+    trace_f = sc_create_vcd_trace_file ("pgcd_2");
+    trace_f->set_time_unit(10,SC_NS);
     sc_trace(trace_f, module_pgcd.clk, "clk");
+    sc_trace(trace_f, module_pgcd.pgcd, "pgcd");
     sc_trace(trace_f, module_pgcd.valid, "valid");
     sc_trace(trace_f, module_pgcd.ready, "ready");
-    A = 45;
-    B = 225;
-    sc_start(100,SC_NS);
-    valid = true;
-    sc_start(10,SC_NS);
+    sc_trace(trace_f, module_pgcd.A, "A");
+    sc_trace(trace_f, module_pgcd.B, "B");
+
+    A = rand() % 200 + 1;
+    B = rand() % 200 + 1;
     valid = false;
-    sc_start(100,SC_NS);
-    A = 60;
-    B = 36;
-    sc_start(100,SC_NS);
-    A = 3072;
-    B = 96;
+    sc_start(40,SC_NS);
     valid = true;
-    sc_start(10,SC_NS);
+    sc_start(40,SC_NS);
     valid = false;
-    sc_start(100,SC_NS);
+    sc_start(40,SC_NS);
+    for (int i = 0 ; i<10000 ; i++){
+        if (ready){
+            cout << "--> @ " << sc_time_stamp() << " pgcd = " << pgcd << endl;
+            A = rand() % 200 + 1;
+            B = rand() % 200 + 1;
+            sc_start(40,SC_NS);
+            valid = true;
+            sc_start(40,SC_NS);
+            valid = false;
+            sc_start(40,SC_NS);
+        }
+        else
+            sc_start(20,SC_NS);
+        
+    }
     sc_close_vcd_trace_file(trace_f);
     return 0;
 }
