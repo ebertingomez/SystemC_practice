ab6ac83f9deb0186a43f7d8e70f3edba48059f7b
Enrique <enrique.gomez@telecom-paristech.fr>
2019-06-05 08:52:16 +0200

    Completion of the RTL model

--- TD5_PGCD/pgcd.cpp
+++ TD5_PGCD/pgcd.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int pgdc(unsigned int a,unsigned int b){
+    if (a == 0 || b == 0)
+        return 0;
     int bigger = (a>b) ? a : b;
     int smaller = (a<b) ? a : b;
     int diff;
