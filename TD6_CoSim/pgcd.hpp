7decdf663efd63026b12b38397188ac34862d97b
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2019-06-05 17:32:26 +0200

    WIP

---
+++ TD6_CoSim/pgcd.hpp
@@ -0,0 +1,19 @@
+#ifndef PGCD_HPP
+#define PGCD_HPP
+#include <stdio.h>
+int compute_pgdc(unsigned int a,unsigned int b){
+    if (a == 0 || b == 0)
+        return 0;
+    int bigger = (a>b) ? a : b;
+    int smaller = (a<b) ? a : b;
+    int diff;
+    do {
+        diff = bigger - smaller;
+        bigger = (diff>smaller) ? diff : smaller;
+        smaller = (diff<smaller) ? diff : smaller;
+    }
+    while (diff >0);
+    return bigger;
+}
+
+#endif PGCD_HPP
